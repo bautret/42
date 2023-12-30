55cdd63b5ee3b01426af0ebc70763d226bfcb0f2
Bastien Autret <39241182+bautret@users.noreply.github.com>
2023-12-30 21:20:12 +0100

    Add files via upload

---
+++ Piscine/C/C00/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bautret <bautret@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 16:44:53 by bautret           #+#    #+#             */
+/*   Updated: 2023/10/02 17:00:34 by bautret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
